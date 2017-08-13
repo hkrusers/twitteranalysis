e8ade9603b844dbb4dedb1883e31e1b1285f706e
tomli1234 <tomli123@hku.hk>
2017-08-13 15:19:51 +0800

    add text cleaning

--- word_cloud.r
+++ word_cloud.r
@@ -1,9 +1,27 @@
 library(wordcloud)
 library(tidyverse)
 
-input_text <- read_csv("tweets.csv")
+# Text cleaning ---------------------------------------------
+tweet <- sapply(input_text$tweet,as.character)
+tweet <- sapply(tweet, function(x) gsub("@\\s.+\\shttp","http",x))  #Remove @place
+tweet <- sapply(tweet, function(x) gsub("htt.*","",x))
+tweet <- sapply(tweet, function(x) gsub("@\\w+", " ", x))  #Remove @User
+tweet <- sapply(tweet, function(x) gsub("#", " ", x))
+tweet <- sapply(tweet, function(x) gsub("\\!", "\\.", x))
+tweet <- sapply(tweet, function(x) gsub("\\?", "\\.", x))
+tweet <- sapply(tweet, function(row) iconv(row, "latin1", "ASCII", sub=""))
+tweet <- clean(tweet)
+tweet <- replace_number(tweet, remove = TRUE)
+tweet <- scrubber(tweet)
+tweet <- incomplete_replace(tweet)
+tweet <- Trim(tweet)
+tweet <- bracketX(tweet,bracket = "all")
+tweet <- add_incomplete(tweet,silent = TRUE)
+tweet <- comma_spacer(tweet)
+tweet <- unname(tweet)
 
-TD_matrix <- input_text	%>%
+# Word cloud---------------------------------------------------
+TD_matrix <- tweet	%>%
 	select(tweet) %>%
 	as.matrix() %>%
 	tm::VectorSource() %>%
