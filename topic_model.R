a13370a0d5a47d7a09069606958b8302ee7b8d46
yuxichau <yuxichau@gmail.com>
2017-08-13 15:53:56 +0800

    Update working topics mined

--- topic_model.R
+++ topic_model.R
@@ -3,6 +3,8 @@ library(data.table)
 library(topicmodels)
 library(tidyverse)
 library(tidytext)
+library(qdap)
+
 
 # Connecting to the database 
 conn <- dbConnect(SQLite(), dbname="tweetdb.sqlite")
@@ -17,22 +19,52 @@ input_text <- data.table(dbGetQuery(conn = db, "select * from tweets"))
 
 input_text <- data.table(read.csv("tweets.csv"))
 
-# Converting tweets Term Document Matrix 
-TD_matrix <- input_text	%>%
-  select(tweet) %>%
-  as.matrix() %>%
-  tm::VectorSource() %>%
-  tm::Corpus() %>%
-  tm::tm_map(tm::removeWords, tm::stopwords("english")) %>%
-  tm::tm_map(tm::removeWords, c("amp", "The", "will", 'You','just', 'https')) %>%
-  tm::TermDocumentMatrix() %>%
-  # tm::removeSparseTerms(sparse = 0.9995) %>%
-  as.matrix()
+# Data Cleaning
+{
+  # Text cleaning ---------------------------------------------
+  tweet <- sapply(input_text$tweet,as.character)
+  tweet <- sapply(tweet, function(x) gsub("@\\s.+\\shttp","http",x))  #Remove @place
+  tweet <- sapply(tweet, function(x) gsub("htt.*","",x))
+  tweet <- sapply(tweet, function(x) gsub("@\\w+", " ", x))  #Remove @User
+  tweet <- sapply(tweet, function(x) gsub("#", " ", x))
+  tweet <- sapply(tweet, function(x) gsub("\\!", "\\.", x))
+  tweet <- sapply(tweet, function(x) gsub("\\?", "\\.", x))
+  tweet <- sapply(tweet, function(row) iconv(row, "latin1", "ASCII", sub=""))
+  tweet <- clean(tweet)
+  tweet <- tolower(tweet)
+  tweet <- replace_number(tweet, remove = TRUE)
+  tweet <- scrubber(tweet)
+  tweet <- incomplete_replace(tweet)
+  tweet <- Trim(tweet)
+  tweet <- bracketX(tweet,bracket = "all")
+  tweet <- add_incomplete(tweet,silent = TRUE)
+  tweet <- comma_spacer(tweet)
+  tweet <- unname(tweet)
+  
+  remove.words <- c("amp", "The", "will", 'You','just', 
+                    'north', 'korea', '...', 'like')
+  
+  # Word cloud---------------------------------------------------
+  TD_matrix <- tweet	%>%
+    #	select(tweet) %>%
+    as.matrix() %>%
+    tm::VectorSource() %>%
+    tm::Corpus() %>%
+    tm::tm_map(tm::removeWords, tm::stopwords("english")) %>%
+    tm::tm_map(tm::removeWords, remove.words) %>%
+    tm::TermDocumentMatrix() %>% 
+    # tm::removeSparseTerms(sparse = 0.9995) %>%
+    as.matrix()
+}
+
 
 # LDA function only takes rows as Docs and columns as Terms, transpose 
 TD_matrix <- t(TD_matrix)
 
-ap_lda <- LDA(TD_matrix, k = 4, control = list(seed = 1234))
+# remove those with 0 row sums
+TD_matrix <- TD_matrix[rowSums(TD_matrix) != 0, ]
+
+ap_lda <- LDA(TD_matrix, k = 3, control = list(seed = 1234))
 
 # Exploring 
 ap_topics <- tidy(ap_lda, matrix = "beta")
